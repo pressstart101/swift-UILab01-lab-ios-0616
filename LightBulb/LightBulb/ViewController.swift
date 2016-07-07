69ba8d6e9257e634a28a4f6805e01f841328802c
pressstart101 <walterscott404@yandex.com>
2016-07-07 14:16:30 -0400

    💡

--- LightBulb/LightBulb/ViewController.swift
+++ LightBulb/LightBulb/ViewController.swift
@@ -14,13 +14,45 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: Change background color of lightBulb view to red
+        
+        changeColor(to: "red".color)
     }
 
     func changeColor(to color: UIColor) {
         // TODO: Change background color to "color"
+        lightBulb.backgroundColor = color
     }
 
     @IBAction func colorSelected(sender: UISegmentedControl) {
+        
+        
+//        if sender.selectedSegmentIndex == 0 {
+//            lightBulb.backgroundColor = "red".color
+//        print("blue selected")
+//        } else if sender.selectedSegmentIndex == 1 {
+//        lightBulb.backgroundColor = "yellow".color
+//        } else if sender.selectedSegmentIndex == 2 {
+//            lightBulb.backgroundColor = "blue".color
+//        } else if sender.selectedSegmentIndex == 3 {
+//            lightBulb.backgroundColor = "green".color
+//        }
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            changeColor(to: "red".color)
+        case 1:
+            changeColor(to: "yellow".color)
+        case 2:
+            changeColor(to: "blue".color)
+        case 3:
+            changeColor(to: "green".color)
+        default:
+            break
+        }
+        
+        
+        //    public func titleForSegmentAtIndex(segment: Int) -> String?
+
         // TODO: Change background color when segmented control changes
     }
 }
